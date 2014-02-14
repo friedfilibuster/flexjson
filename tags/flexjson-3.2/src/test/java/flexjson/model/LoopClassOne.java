72691770978a49921ca4ba83db1c81de566124b5
charliehubbard <charliehubbard@1649a90f-f54c-0410-9814-d0bc849eed40>
2014-02-14 03:06:55 +0000

    [maven-release-plugin]  copy for tag flexjson-3.2

---
+++ tags/flexjson-3.2/src/test/java/flexjson/model/LoopClassOne.java
@@ -0,0 +1,21 @@
+package flexjson.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: brandongoodin
+ * Date: Aug 19, 2008
+ * Time: 5:32:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoopClassOne {
+
+    private LoopClassTwo loopClassTwo;
+
+    public LoopClassTwo getLoopClassTwo() {
+        return loopClassTwo;
+    }
+
+    public void setLoopClassTwo(LoopClassTwo loopClassTwo) {
+        this.loopClassTwo = loopClassTwo;
+    }
+}
